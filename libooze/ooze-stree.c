a3d7ada7bf5b473bd8becd1fef2fe24a0fff6ae1
przemyslaw.sitek <przemyslaw.sitek@16401168-1fa4-11de-a108-cd2f117ce590>
2009-05-01 16:23:18 +0000

    Initial OoozeSTree implementation

---
+++ libooze/ooze-stree.c
@@ -0,0 +1,63 @@
+/*
+ * elpea
+ * -----
+ *
+ * Copyright (C) 2009 Przemysław Sitek
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include "ooze-stree.h"
+
+OozeSTree *
+ooze_stree_new (void)
+{
+	OozeSTree *self = g_new0 (OozeSTree, 1);
+	return self;
+}
+
+
+void
+ooze_stree_free (OozeSTree *self)
+{
+	g_free (self);
+}
+
+
+void
+ooze_stree_add (OozeSTree   *self,
+                const gchar *suffix,
+                gint         len)
+{
+}
+
+
+gboolean
+ooze_stree_ends_with (OozeSTree   *self,
+                      const gchar *subject,
+                      gint         len)
+{
+	return TRUE;
+}
+
+
+
