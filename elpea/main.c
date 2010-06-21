9b623c9258da4c0129110a6ff17e388a7672809a
przemyslaw.sitek <przemyslaw.sitek@16401168-1fa4-11de-a108-cd2f117ce590>
2010-06-21 20:41:14 +0000

    Preferences support [core]

--- elpea/main.c
+++ elpea/main.c
@@ -3,14 +3,17 @@
 #include <gtk/gtk.h>
 
 #include "elpea-main-window.h"
+#include "foo-prefs-gconf.h"
 
-
+FooPrefs *prefs = NULL;
 
 int main (int argc, char *argv[])
 {
 	g_thread_init (NULL);
 	gtk_init (&argc, &argv);
 	gtk_gl_init (&argc, &argv);
+
+	prefs = FOO_PREFS (foo_prefs_gconf_new ());
 
 	GtkWidget *window = elpea_main_window_new ();
 
